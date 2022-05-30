aab7dd0b361d81aed34d5b9eff9ae010e4b19abe
Hakan Baglan <baglanhakan42@gmail.com>
2022-05-30 21:55:22 +0300

    project started

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'firebase-busines-web-site-width-admin-panel';
+}
